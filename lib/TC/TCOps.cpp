1ea9ea979be4df566580cc123e206149c20feaae
Braden Helmer <bradenhelmeraus@gmail.com>
2026-07-28 20:59:40 -0400

    type and verifier

--- lib/TC/TCOps.cpp
+++ lib/TC/TCOps.cpp
@@ -6,4 +6,19 @@
 using namespace mlir;
 using namespace mlir::tc;
 
-LogicalResult MatMulOp::verify() { return success(); }
+LogicalResult MatMulOp::verify() {
+
+  auto lhsShape = getLhs().getType().getShape();
+  auto rhsShape = getRhs().getType().getShape();
+
+  if (lhsShape.size() != 2 || rhsShape.size() != 2) {
+    return emitOpError("MatMulOp requires two-dimensional tensors");
+  }
+
+  if (lhsShape[1] != rhsShape[0]) {
+    return emitOpError(
+        "MatMulOp requires shape compatibility: (M,K) x (K,N) -> (M,N)");
+
+    return success();
+  }
+}
